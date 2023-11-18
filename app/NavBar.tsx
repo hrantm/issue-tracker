5c3306d954390cad38fec5367b5ea305e22c643d
hrantm <hrant.mirijanyan@gmail.com>
2023-11-18 05:39:35 -0800

    set up classnames for navbar highlighting

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,26 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
-
+import classnames from 'classnames';
+import { usePathname } from 'next/navigation';
 
 const NavBar = () => {
     const listItems = [
         {path: '/', label: 'Dashboard'},
         {path: '/issues', label: 'Issues'}
     ]
+    const currentPath = usePathname()
     return (
     <nav className='flex space-x-6 items-center border-b mb-5 px-5 h-14'>
         <Link href="/"><AiFillBug/></Link>
         <ul className='flex space-x-6'>
             {listItems.map(item => (
-                <li key={item.path}><Link href={item.path} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{item.label}</Link></li>
+                <li key={item.path}><Link href={item.path} className={classnames({
+                    'text-zinc-900': item.path === currentPath,
+                    'text-zinc-500': item.path !== currentPath,
+                    'hover:text-zinc-800': true
+                })}>{item.label}</Link></li>
             ))}
         </ul>
 
