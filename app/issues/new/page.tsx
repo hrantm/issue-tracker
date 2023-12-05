923fe2209806426f49b89c3540704b8c94ece529
hrantm <hrant.mirijanyan@gmail.com>
2023-12-05 08:18:16 -0800

    Use react hook form to send post request

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,36 @@
 'use client';
 import { Button, TextArea, TextField } from '@radix-ui/themes'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React from 'react'
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type IssueForm = {
+    title: string
+    description: string
+}
 
 const newIssue = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder="Title" />
-        </TextField.Root>
-        <TextArea placeholder="Description" />
-        <Button>Submit New Issue</Button>
-    </div>
-  )
+    const router = useRouter();
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+        } = useForm<IssueForm>()    
+    const onSubmit: SubmitHandler<IssueForm> = async (data) => {
+        await axios.post('/issues/new', data)
+        router.push('/issues')
+    }
+    return (
+        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(onSubmit)}>
+            <TextField.Root>
+                <TextField.Input placeholder="Title" {...register("title")}/>
+            </TextField.Root>
+            <TextArea placeholder="Description" {...register("description")}/>
+            <Button>Submit New Issue</Button>
+        </form>
+    )
 }
 
 export default newIssue
