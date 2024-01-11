9f312e5f1b760cd8e0ef4c3edc5c2c70c52fd8d9
hrantm <hrant.mirijanyan@gmail.com>
2024-01-11 14:36:14 -0800

    add issue delete endpoint

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -33,3 +33,22 @@ export async function PATCH(request: NextRequest,
     })
     return NextResponse.json(updatedIssue, {status: 200})
 }
+
+
+export async function DELETE(request: NextRequest,
+    {params}: { params: {id: string}}){
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    })
+    if(!issue){
+        return NextResponse.json({error: 'Inavlid Issue'}, {status: 404})
+    }
+    await prisma.issue.delete({
+        where: {
+            id: issue.id
+        }        
+    })
+    return NextResponse.json({})
+}
