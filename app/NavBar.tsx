f572fa5edaf21f1a1324f1fd8ee25b737722d97e
hrantm <hrant.mirijanyan@gmail.com>
2024-01-31 15:14:34 -0800

    Add dropdown for login/logout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { AiFillBug } from "react-icons/ai";
 import classnames from 'classnames';
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
     const {status, data: session} = useSession();
@@ -31,7 +31,20 @@ const NavBar = () => {
                     </ul>                
                 </Flex>
                 <Box>
-                    {status === 'authenticated' && <Link href='/api/auth/signout'>Logout</Link>}
+                    {status === 'authenticated' && (
+                        <DropdownMenu.Root>
+                             <DropdownMenu.Trigger>
+                                <Avatar fallback='?' size='2' radius='full' src={session.user!.image!}/>
+                             </DropdownMenu.Trigger>
+                             <DropdownMenu.Content>                                
+                                <DropdownMenu.Label>
+                                    <Text size='2'>{session.user?.email}</Text>
+                                </DropdownMenu.Label>
+                                <DropdownMenu.Item><Link href='/api/auth/signout'>Logout</Link></DropdownMenu.Item>
+                             </DropdownMenu.Content>
+                             
+                        </DropdownMenu.Root>
+                    )}
                     {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
                 </Box>
             </Flex>
