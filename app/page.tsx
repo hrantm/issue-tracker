4b1286bca60dc35d15cfd7f9ac01a5d068f26160
hrantm <hrant.mirijanyan@gmail.com>
2024-02-13 06:14:59 -0800

    latestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
-export default function Home({searchParams}: { searchParams: {page: string}}) {
+export default function Home() {
   return (
-    <Pagination currentPage={parseInt(searchParams.page)} pageSize={10} itemCount={100}/>
-
+    <LatestIssues/>
   )
 }
